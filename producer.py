d42407f4af565c9cac617951045d6b282caf1f13
Svalker1989 <117890145+Svalker1989@users.noreply.github.com>
2023-10-18 16:14:52 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
 import pika
+# логин и пароль от УЗ, под которой будем работать
 credentials = pika.PlainCredentials('str', 'qwerty')
+# ip адрес и порт rabbitmq
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.148',5672,'/',credentials))
 channel = connection.channel()
+#Создаем очередь
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
 connection.close()
